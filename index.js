dbdca9da88e7339d21be34926ee2076499f368d0
Will Jones <willjones2010@gmail.com>
2019-03-04 22:10:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ function theBeatlesPlay(musicians,instruments) {
   return newArray;
 }
 function johnLennonFacts(facts) {
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 var i = 0
 newArray = [];
 while (i < facts.length) {
