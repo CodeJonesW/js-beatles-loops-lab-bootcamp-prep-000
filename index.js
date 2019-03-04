49a218e949209a4fc55a17bfe08e09d4e3d287b9
Will Jones <willjones2010@gmail.com>
2019-03-04 22:18:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ return newArray;
 }
 function iLoveTheBeatles (num) {
   let newArray = [];
-  do {newArray.push('I love the Beatles!') num++} while (num < 15);
+  do {newArray.push('I love the Beatles!'); num++} while (num < 15);
   return newArray;
 }
 
